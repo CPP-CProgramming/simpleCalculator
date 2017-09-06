2731ccb620f95d51dde784e0da9cb5a2482f4567
inchan hwang <neogeo-s@hanmail.net>
2017-09-06 22:44:17 +0900

    Good

---
+++ simpleCalculator/InputConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleCalculator
+{
+    public class InputConverter
+    {
+        public double ConvertInputToNumber(string argTextInput)
+        {
+            double convertedNumber;
+            if (!double.TryParse(argTextInput, out convertedNumber)) throw new ArgumentException("Expected a numeric value");
+            return convertedNumber;
+        }
+    }
+}
---
+++ simpleCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                double firstNumber = inputConverter.ConvertInputToNumber(Console.ReadLine());
+                double secondNumber = inputConverter.ConvertInputToNumber(Console.ReadLine());
+                string operation = Console.ReadLine();
+
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                //Start logging exception
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
